1f2774f3a0881a620c6cd15262ca2495e2b0d512
Luiz Sena <senaluiz1307@gmail.com>
2022-10-11 10:45:50 -0300

    first commit

---
+++ AnimeListApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  AnimeListApp
+//
+//  Created by Luiz Sena on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
